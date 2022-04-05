282308c2b9143d40cdde1300d74afee4a1f74312
Roman Ojha <razzroman98@gmail.com>
2022-04-05 19:26:02 +0545

    change some stuff on use sass with NextJS tut

--- Tutorial/04_sass_with_nextjs/pages/index.js
+++ Tutorial/04_sass_with_nextjs/pages/index.js
@@ -1,4 +1,4 @@
-import styles from "../styles/Home.module.scss";
+import styles from "../styles/pages/Home.module.scss";
 
 export default function Home() {
   return <div className={styles.container}></div>;
