bfbbf080735878eac44afd9345dd64a328f30caa
Roman Ojha <razzroman98@gmail.com>
2022-04-05 19:22:44 +0545

    sass tut on use sass in nextJS app

---
+++ Tutorial/04_sass_with_nextjs/04_Sass_With_NextJS.js
@@ -0,0 +1,5 @@
+/*
+    -> yarn create next-app <project_name>
+    -> yarn add sass
+    => To use sass in nextJS you just need to change the name form css modules and css file into 'scss' file 
+*/
